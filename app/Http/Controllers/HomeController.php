a0126ed19df1b64bf083b65a213a6834f7ca0023
diman-dn <vagrant@laravel.dev>
2018-11-03 08:30:36 +0000

    Move category block to the partials; Added view, action and route for pages by tag and by category; Added new methods in Post model;

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,24 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
+use App\Tag;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    /**
+     * Main page action
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
-        $posts = Post::paginate(5);
+        $posts = Post::where('status', 0)->paginate(5);
         return view('pages.index', [
             'posts' => $posts
         ]);
     }
 
+    /**
+     * Single post action
+     * @param $slug
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show($slug)
     {
-        $post = Post::where('slug', $slug)->firstOrFail();
+        $post = Post::where('status', 0)->where('slug', $slug)->firstOrFail();
         return view('pages.show', [
             'post' => $post
+        ]);
+    }
+
+    public function tag($slug)
+    {
+        $tag = Tag::where('slug', $slug)->firstOrFail();
+        $posts = $tag->posts()->where('status', 0)->paginate(6);
+        return view('pages.list', [
+            'posts' => $posts
+        ]);
+    }
+
+    public function category($slug)
+    {
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $posts = $category->posts()->where('status', 0)->paginate(6);
+        return view('pages.list', [
+            'posts' => $posts
         ]);
     }
 }
